cf245a1dcb5ae3a3beebc441c0db9122404150bd
Hitesh <hiteshspatil99@gmail.com>
2021-11-30 00:47:23 +0530

    [Hitesh] add : To Validate Last Name Using Regex

--- CreateRegexProgram/CreateRegexProgram/Program.cs
+++ CreateRegexProgram/CreateRegexProgram/Program.cs
@@ -7,24 +7,31 @@ namespace CreateRegexProgram
     {
         static void Main(string[] args)
         {
+            RegexPatterns checkpattern = new RegexPatterns();
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Choose Option to Execute the Regex Program: \n 1. Validate First Name, \n 2. Exit");
+                Console.WriteLine("User Registration Validate using Regular Expression");
+                Console.WriteLine("Choose Option to Execute the Regex Program: \n 1. Validate First Name, \n 2. Validate Last Name, \n 3. Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
-                        Console.WriteLine("User Registration Validate using Regular Expression");
-
-                        RegexPatterns checkpattern = new RegexPatterns();
+                        
                         Console.WriteLine("For Check Validation Enter First Name");
                         string input = Console.ReadLine();
                         checkpattern.ValidateFirstName(input);
-                        checkpattern.Validating();
+                        checkpattern.Validating(input);
                         break;
 
                     case 2:
+                        Console.WriteLine("For Check Validation Enter Last Name");
+                        string inputLastname = Console.ReadLine();
+                        checkpattern.ValidateLastName(inputLastname);
+                        checkpattern.Validating(inputLastname);
+                        break;
+
+                    case 3:
                         flag = false;
                         break;
 
--- CreateRegexProgram/CreateRegexProgram/RegexPatterns.cs
+++ CreateRegexProgram/CreateRegexProgram/RegexPatterns.cs
@@ -9,6 +9,14 @@ namespace CreateRegexProgram
     public class RegexPatterns
     {
         string pattern = "^[A-Za-z]{2,}$";
+            public void Validating(string input)
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Validating the First Name");
+            ValidateFirstName(input);
+            Console.WriteLine("Validating the Last Name");
+            ValidateLastName(input);
+        }
         public void ValidateFirstName(string input)
         {
             Regex regex = new Regex(pattern);
@@ -23,11 +31,18 @@ namespace CreateRegexProgram
                 Console.WriteLine("Invalid Name");
             }
         }
-        public void Validating()
+        public void ValidateLastName(string inputLastname)
         {
             Regex regex = new Regex(pattern);
-            Console.WriteLine("First Name Validating");
+            bool result = regex.IsMatch(inputLastname);
+            if (result)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
         }
     }
 }
-     
