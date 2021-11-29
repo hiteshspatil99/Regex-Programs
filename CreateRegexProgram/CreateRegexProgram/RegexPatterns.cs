253ac0af64b052587df36a8e88eb7ccbb9d6c452
Hitesh <hiteshspatil99@gmail.com>
2021-11-29 09:30:38 +0530

    [Hitesh] add : To Validate First Name Using Regex

---
+++ CreateRegexProgram/CreateRegexProgram/RegexPatterns.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CreateRegexProgram
+{
+    public class RegexPatterns
+    {
+        string pattern = "^[A-Za-z]{2,}$";
+        public void ValidateFirstName(string input)
+        {
+            Regex regex = new Regex(pattern);
+
+            bool res = regex.IsMatch(input);
+            if (res)
+            {
+                Console.WriteLine("Valid Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name");
+            }
+        }
+        public void Validating()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("First Name Validating");
+        }
+    }
+}
+     
